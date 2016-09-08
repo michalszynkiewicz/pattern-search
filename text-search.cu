f26a364c44eb525a1f11a376e1b88bd367bd6751
Michal Szynkiewicz <michal.l.szynkiewicz@gmail.com>
2016-09-08 23:09:37 +0200

    added performance results + set to 10x1000

--- text-search.cu
+++ text-search.cu
@@ -6,8 +6,8 @@
 #include "MultiplierHandler.h"
 #include "PatternHandler.h"
 
-#define THREAD_COUNT 10
-#define DIM_COUNT 100
+#define THREAD_COUNT 1000
+#define DIM_COUNT 10
 #define MAX_PATTERN_LENGTH 20
 #define MULTIPLIER 1117
 #define MODULUS 2147483647
