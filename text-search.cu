c911390687f9061ecc9ed43ed24fb2cf9899717b
Michal Szynkiewicz <michal.l.szynkiewicz@gmail.com>
2016-09-08 22:14:13 +0200

    10x100

--- text-search.cu
+++ text-search.cu
@@ -6,7 +6,7 @@
 #include "MultiplierHandler.h"
 #include "PatternHandler.h"
 
-#define THREAD_COUNT 10
+#define THREAD_COUNT 100
 #define DIM_COUNT 10
 #define MAX_PATTERN_LENGTH 20
 #define MULTIPLIER 1117
